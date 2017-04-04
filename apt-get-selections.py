7579c2629b5cf6b0edb59fb9139be43514388892
setMinami <set.minami@gmail.com>
2017-04-04 14:43:51 +0900

    argparse組み込み

--- apt-get-selections.py
+++ apt-get-selections.py
@@ -2,14 +2,21 @@
 # -*- coding: utf-8 -*-
 # this made for python3
 import subprocess as sp
+import posix, os
+import argparse
 import re
 
 class AptgetSelection:
     """ """
     DEBUG = True
+    VERSIONNUM = '1.0'
+    VERSION = 'AptgetSelection %s'%VERSIONNUM
 
-    def __init__(self, dpkg_lFile='../dpkg-l.txt'):
-        self.pkgedFile = dpkg_lFile
+    def __init__(self, args=None, dpkg_lFile='../dpkg-l.txt'):
+        if args is not None:
+            self.pkgedFile = args.package
+        else:
+            self.pkgedFile = dpkg_lFile
         self.pkgformat = '\\w,\\d,\.,\-,\:,\+'
         pass
 
@@ -51,6 +58,22 @@ def _print(str, flag=AptgetSelection.DEBUG):
 
 
 if __name__ == '__main__':
-    ins = AptgetSelection()
-    ins.do()
-    ins.finalize()
+    argParser = argparse.ArgumentParser(prog=__file__,
+                                        description='TBD',
+                                        usage='./apt-get-selection.py [options]')
+    # Version desctiprtion
+    argParser.add_argument('-v', '--version',
+                            action='version',
+                            version='%s'%AptgetSelection.VERSION)
+    argParser.add_argument('-p', '--package',
+                            nargs='?', type=str, default='../dpkg-l.txt',
+                            help='formats on still Images for Timelapse')
+    argParser.add_argument('-rS', '--resetSession',
+                                action='store_true', help='Maker Event is on.')
+    args = argParser.parse_args()
+    uname = os.uname()
+    print('system on %s'%uname[0])
+    if isinstance(os.uname, posix.uname_result) and uname[0] == 'Linux':
+        ins = AptgetSelection(args)
+        ins.do()
+        ins.finalize()
